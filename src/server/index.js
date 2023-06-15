c7c05f1fc5409eae2d7fa088170929718c0697c4
Matthew Bell <m.r.bell@outlook.com>
2023-06-15 20:58:39 +0100

    amends to server for render deploy

--- src/server/index.js
+++ src/server/index.js
@@ -29,9 +29,16 @@ console.log(__dirname);
 
 // get request
 
+// app.get("/", function (req, res) {
+//   res.sendFile("dist/index.html");
+//   res.sendFile(path.resolve("src/client/views/index.html"));
+// });
+
+//test for render
+
 app.get("/", function (req, res) {
-  res.sendFile("dist/index.html");
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  const filePath = path.resolve(__dirname, "src/client/views/index.html");
+  res.sendFile(filePath);
 });
 
 // designates what port the app will listen to for incoming requests
